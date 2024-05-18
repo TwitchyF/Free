fc86e47f62cf89b4274b1208a6a736e26d632b56
Rikikangsc2 <112865967+Rikikangsc2@users.noreply.github.com>
2024-05-18 01:52:34 +0000

    Modifev

--- fitur/func/search-image.js
+++ fitur/func/search-image.js
@@ -1,34 +1,19 @@
 var gis = require('g-i-s');
-var axios = require('axios');
-var sharp = require('sharp');
 
 const get = (query) => {
   return new Promise((resolve, reject) => {
-    gis(query, async (error, results) => {
+    gis(query, (error, results) => {
       if (error) {
         reject(error);
       }
       else {
-        let promises = results.map(result => checkImage(result.url));
-        let urls = await Promise.all(promises);
-        urls = urls.filter(url => url !== null); // Filter out null values
+        let urls = results
+          .filter(result => result.width >= 800 && result.height >= 600) // Check if the image is HD
+          .map(result => result.url); // Map to array of URLs
         resolve(urls);
       }
     });
   });
-}
-
-const checkImage = async (url) => {
-  try {
-    let response = await axios.get(url, { responseType: 'arraybuffer' });
-    let metadata = await sharp(response.data).metadata();
-    if(metadata.width >= 1280 && metadata.height >= 720) { // Check if the image is HD
-      return url;
-    }
-  } catch(err) {
-    console.error(err);
-  }
-  return null;
 }
 
 module.exports = { get };
