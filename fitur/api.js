3ae54a20b2edf26f62f2faa5243bef541e5e2356
Rikikangsc2 <112865967+Rikikangsc2@users.noreply.github.com>
2024-07-01 11:46:47 +0000

    B

--- fitur/api.js
+++ fitur/api.js
@@ -10,7 +10,7 @@ const ytdl = require("ytdl-core");
 
 router.get("/text2img", async (req, res) => {
   const { prompt, model } = req.query;
-  if (!prompt || !model) return res.status(400).send(`Pastikan prompt dan model terisi, untuk melihat daftar model bisa akses <a href="https://tattered-classy-comic.glitch.me/sdlist" target="_blank">List berikut</a>`);
+  if (!prompt || !model && model === "MODELNYA-ISI-DONG") return res.status(400).send(`Pastikan prompt dan model terisi, untuk melihat daftar model bisa akses <a href="https://tattered-classy-comic.glitch.me/sdlist" target="_blank">List berikut</a>`);
   res.redirect(`https://tattered-classy-comic.glitch.me/text2img?model=${model}&prompt=${encodeURIComponent(prompt)}`);
 });
 
